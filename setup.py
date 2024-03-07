b132e3b1e972859e9f4d5997edd1d0d5fb6d0b6e
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2024-03-08 01:07:12 +0400

    set up py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import find_packages, setup
+from typing import List
 
+HYPEN_E ='-e .'
+def get_requirement(file_path: str) -> List[str]:
+    requirments = []
+    with open(file_path) as file_obj:
+        requirments= file_obj.readline()
+        requirments= [req.replace("\n","") for req in requirments]
+
+        if HYPEN_E in requirments:
+            requirments.remove(HYPEN_E)
+    return requirments
 
 setup(
-    name='mlproject',
+    name='ml-personal-project',
     version='0.0.1',
     author='jagmeet',
     author_email='jagmeetghotra2002@gmail.com',
     packages=find_packages(),
-    install_requires=['pandas','numpy','seaborn']
+    install_requires=get_requirement('requirments.txt')
 )
