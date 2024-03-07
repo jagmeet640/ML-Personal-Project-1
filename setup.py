4473d757fe3bc692a09f280fbaf9f12eebe59f35
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2024-03-08 00:47:04 +0400

    setup py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='jagmeet',
+    author_email='jagmeetghotra2002@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+)
