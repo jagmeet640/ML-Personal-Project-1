ae4fb983d77fc8e8d196a461b965db89413124e5
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2024-03-10 00:24:36 +0400

    added secrets and added sql connection code AWS

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-import pandas
+import pymysql
+import toml
+
+config = toml.load('secret.toml')
+
+def test_connection():
+    try:
+        # Establish a connection to the database
+        connection = pymysql.connect(
+            host= config['database']['host'],
+            user= config['database']['username'],
+            password= config['database']['password'],
+            database= config['database']['database'],
+            port= config['database']['port']
+        )
+        
+        # If connection is successful, print a success message
+        print("Connection successful!")
+        
+        # Close the connection
+        connection.close()
+        
+    except Exception as e:
+        # If connection fails, print the error message
+        print("Connection failed:", str(e))
+
+# Call the test_connection function to test the connection
+test_connection()
