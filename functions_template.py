874f3004d7991f64ac6a33484f7b87ab6e2b1f86
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2024-03-14 00:00:44 +0400

    Templates made functions built

--- functions_template.py
+++ functions_template.py
@@ -1,9 +1,84 @@
+import pymysql
+import toml
+
+config = toml.load('secrets.toml')
+
 # document for the basic work flow of all the functions for the RDBMs
 
 ## Basic DB functions 
 
 # Funtion1 ~ review employee data
+def getAllEmpData():
+
+    myConnection = pymysql.connect(
+        host= config['database']['host'],
+        user= config['database']['username'],
+        password= config['database']['password'],
+        database= config['database']['database'],
+        port= config['database']['port']
+    )
+
+    cursor = myConnection.cursor()
+
+    sql_query = "SELECT * from employee"
+
+    employee_info = cursor.execute(sql_query)
+
+    myConnection.commit()
+
+    cursor.close()
+    myConnection.close()
+
+    return employee_info
 
 # Function2 ~ review employee data (filter by designation)
 
+def getDesgEmpData(designation):
+
+    myConnection = pymysql.connect(
+        host= config['database']['host'],
+        user= config['database']['username'],
+        password= config['database']['password'],
+        database= config['database']['database'],
+        port= config['database']['port']
+    )
+
+    cursor = myConnection.cursor()
+
+    sql_query = "SELECT * from Employee where designation = %s"
+
+    employee_info = cursor.execute(sql_query, designation)
+
+    myConnection.commit()
+
+    cursor.close()
+
+    myConnection.close()
+
+    return employee_info
+
 # Fuction3 ~ review empoloyee data (filter by dept)
+
+def getDeptEmpData(dept):
+
+    myConnection = pymysql.connect(
+        host= config['database']['host'],
+        user= config['database']['username'],
+        password= config['database']['password'],
+        database= config['database']['database'],
+        port= config['database']['port']
+    )
+
+    cursor = myConnection.cursor()
+
+    sql_query = "SELECT * from Employee where department = %s"
+
+    employee_info = cursor.execute(sql_query, dept)
+
+    myConnection.commit()
+
+    cursor.close()
+
+    myConnection.close()
+
+    return employee_info 
