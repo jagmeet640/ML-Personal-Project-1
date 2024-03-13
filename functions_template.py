4e9e02ab5ec14b67e355844e815d767ed614e2b0
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2024-03-13 23:49:56 +0400

    functions template created

---
+++ functions_template.py
@@ -0,0 +1,9 @@
+# document for the basic work flow of all the functions for the RDBMs
+
+## Basic DB functions 
+
+# Funtion1 ~ review employee data
+
+# Function2 ~ review employee data (filter by designation)
+
+# Fuction3 ~ review empoloyee data (filter by dept)
