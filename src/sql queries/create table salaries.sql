34bd989b7a1e7679f85876bea0ac6bc5eff090ee
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2024-03-14 01:20:57 +0400

    data base tabl for salaries

---
+++ src/sql queries/create table salaries.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Company.Salaries (
+    EmpID INT PRIMARY KEY,
+    HourlyWage DECIMAL(10, 2),
+    Hours_Per_month INT,
+    OverTime_Hours_Per_month INT
+);
---
+++ src/sql queries/insert into salaries DummyData.sql
@@ -0,0 +1,5 @@
+INSERT INTO Company.Salaries (EmpID, HourlyWage, Hours_Per_month, OverTime_Hours_Per_month) VALUES
+(1, 15.50, 160, 10),
+(2, 20.25, 160, 15),
+(3, 18.75, 160, 5),
+(4, 22.00, 160, 20);
