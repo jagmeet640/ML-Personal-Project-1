adc6a5129154930750c7e1bf96bdebc8edb59e1b
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2024-03-18 18:42:05 +0400

    added function in template

--- src/functions_template.py
+++ src/functions_template.py
@@ -88,3 +88,35 @@ def getDeptEmpData(dept):
     myConnection.close()
 
     return employee_info 
+
+# Function4 ~ create calculated salaries table and save into a dataframe
+
+def salariesEmployeeJoin():
+    myConnection = pymysql.connect(
+        host= config['database']['host'],
+        user= config['database']['username'],
+        password= config['database']['password'],
+        database= config['database']['database'],
+        port= config['database']['port']
+    )
+
+    cursor = myConnection.cursor()
+
+    sql_query = "select Company.Employee.EmpID as Id, Company.Employee.name, Company.Employee.Department, Company.Employee.Post, Company.Salaries.HourlyWage * Company.Salaries.Hours_Per_month as Base,Company.Salaries.HourlyWage * 1.5 * Company.Salaries.OverTime_Hours_Per_month as Overtime,(Company.Salaries.HourlyWage * Company.Salaries.Hours_Per_month) + (Company.Salaries.HourlyWage * 1.5 * Company.Salaries.OverTime_Hours_Per_month) as Total from Company.Employee left join Company.Salaries on Company.Employee.EmpID = Company.Salaries.EmpID"
+
+    cursor.execute(sql_query)
+
+    result = cursor.fetchall()
+
+    myConnection.commit()
+
+    cursor.close()
+
+    myConnection.close
+
+    return result
+
+
+print(salariesEmployeeJoin())
+
+
