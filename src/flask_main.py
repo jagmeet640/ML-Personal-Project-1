5ce6fc379973c6b15dec2cef6b55bffa80ddb805
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2024-03-31 01:09:55 +0400

    feature/added route to ender employee and backend

--- src/flask_main.py
+++ src/flask_main.py
@@ -3,6 +3,10 @@
 from flask import Flask, render_template, jsonify
 
 import requests
+from flask import Flask, render_template, request, redirect, url_for
+
+import requests
+
 
 app = Flask(__name__)
 
@@ -17,6 +21,28 @@ def get_employees():
     # Make a request to your FastAPI endpoint to fetch employee data
     response = requests.get('http://127.0.0.1:8000/employees/')
     return jsonify(response.json())
+
+@app.route('/AddEmployeePage')
+def get_addEmployeePage():
+    return render_template('index_add_emp.html')
+
+@app.route('/addEmployees', methods=['POST', 'GET'])
+def add_employee():
+    # Get employee data from form
+    employee_data = {
+        'EmpID': request.form['EmpID'],
+        'name': request.form['name'],
+        'age': request.form['age'],
+        'number': request.form['number'],
+        'department': request.form['department'],
+        'post': request.form['post']
+    }
+    # Send POST request to FastAPI endpoint to add employee
+    response = requests.post('http://127.0.0.1:8000/employeeEnter/', json=employee_data)
+    if response.status_code == 200:
+        return redirect(url_for('view'))  # Redirect to view page if employee added successfully
+    else:
+        return "Failed to add employee"
 
 @app.route('/view')
 def view():
