a6388586ce4b49c17d4f7550588f0c02e9b1d0dc
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2024-04-07 18:07:03 +0400

    feature/css added

--- src/flask_main.py
+++ src/flask_main.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # Define routes to serve HTML templates
 @app.route('/')
 def index():
-    return render_template('index2.html')
+    return render_template('index.html')
 
 
 @app.route('/AddEmployeePage')
@@ -33,46 +33,50 @@ def get_employees():
     return jsonify(response.json())
 
 
+@app.route('/view')
+def view():
+    # employee_data = requests.get('http://127.0.0.1:8000/employees/')
+    # employee_data = employee_data.json()
+    # return render_template('index_view.html', employee_data=employee_data)
+    return render_template('index_view.html')
+
+
 ## Data base functionality routes :
 
 
-@app.route('/addEmployees', methods=['POST', 'GET'])
-def add_employee():
-    # Get employee data from form
-    employee_data = {
-        'EmpID': request.form['EmpID'],
-        'name': request.form['name'],
-        'age': request.form['age'],
-        'number': request.form['number'],
-        'department': request.form['department'],
-        'post': request.form['post']
-    }
-    # Send POST request to FastAPI endpoint to add employee
-    response = requests.post('http://127.0.0.1:8000/employeeEnter/', json=employee_data)
-    if response.status_code == 200:
-        return redirect(url_for('view'))  # Redirect to view page if employee added successfully
-    else:
-        return "Failed to add employee"
+# @app.route('/addEmployees', methods=['POST', 'GET'])
+# def add_employee():
+#     # Get employee data from form
+#     employee_data = {
+#         'EmpID': request.form['EmpID'],
+#         'name': request.form['name'],
+#         'age': request.form['age'],
+#         'number': request.form['number'],
+#         'department': request.form['department'],
+#         'post': request.form['post']
+#     }
+#     # Send POST request to FastAPI endpoint to add employee
+#     response = requests.post('http://127.0.0.1:8000/employeeEnter/', json=employee_data)
+#     if response.status_code == 200:
+#         return redirect(url_for('view'))  # Redirect to view page if employee added successfully
+#     else:
+#         return "Failed to add employee"
 
 
 
-@app.route('/deleteEmployee', methods=['POST', 'GET'])
-def deleteEmployee():
-    employee_name = request.form['name']
-    requests.delete('http://127.0.0.1:8000/deleteEmployee/', employee_name)
+# @app.route('/deleteEmployee', methods=['POST', 'GET'])
+# def deleteEmployee():
+#     employee_name = request.form['name']
+#     requests.delete('http://127.0.0.1:8000/deleteEmployee/', employee_name)
 
 
-@app.route('/view')
-def view():
-    employee_data = requests.get('http://127.0.0.1:8000/employees/')
-    employee_data = employee_data.json()
-    return render_template('index_view.html', employee_data=employee_data)
 
-@app.route('/api/salaries')
-def get_salaries():
-    # Make a request to your FastAPI endpoint to fetch salary data
-    response = requests.get('http://127.0.0.1:8000/Salaries/')
-    return jsonify(response.json())
+
+# @app.route('/api/salaries')
+# def get_salaries():
+#     # Make a request to your FastAPI endpoint to fetch salary data
+#     response = requests.get('http://127.0.0.1:8000/Salaries/')
+#     return jsonify(response.json())
 
 if __name__ == '__main__':
     app.run(debug=True)
