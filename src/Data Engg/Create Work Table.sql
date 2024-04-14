783648289328d1dc46639d35669d1e977f2c5448
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2024-04-14 20:51:45 +0400

    Database/ created a work table to have details

---
+++ src/Data Engg/Create Work Table.sql
@@ -0,0 +1,8 @@
+Create Table Company.Work(
+	EmpID INT,
+    name varchar(225),
+    month varchar(225),
+    hoursWorked INT,
+    offDays INT,
+    leaveDays INT
+);
---
+++ src/Data Engg/insert into Work table.sql
@@ -0,0 +1,5 @@
+INSERT INTO Company.Work (EmpID, name, month, hoursWorked, offDays, leaveDays)
+VALUES (1, 'John Doe', '2022-01', 50, 0, 0),
+       (2, 'Jane Smith', '2021-12', 30, 0, 0),
+       (3, 'Michael Johnson', '2022-02', 45, 0, 0),
+       (9398, 'Jagmeet Ghotra', '2022-03', 60, 0, 0);
