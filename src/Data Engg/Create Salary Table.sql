0b1589133fa047a178623cabe8628816aa7e04dc
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2024-04-14 20:06:44 +0400

    Database/ created salary table and populated

---
+++ src/Data Engg/Create Salary Table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Company.Salary (
+    EmpID INT,
+    Name VARCHAR(255),
+    JoinDate DATE,
+    HourlyRate DECIMAL(10, 2), -- Assuming a decimal data type for hourly rate with precision 10 and scale 2
+    BaseSalary DECIMAL(10, 2)  -- Assuming a decimal data type for base salary with precision 10 and scale 2
+);
---
+++ src/Data Engg/insert into Salary table.sql
@@ -0,0 +1,5 @@
+INSERT INTO Company.Salary (EmpID, Name, JoinDate, HourlyRate, BaseSalary) VALUES
+(1, 'John Doe', '2022-01-01', 50.00, 50000.00),
+(2, 'Jane Smith', '2021-12-15', 30.00, 40000.00),
+(3, 'Michael Johnson', '2022-02-28', 45.00, 55000.00),
+(9398, 'Jagmeet Ghotra', '2022-03-10', 60.00, 65000.00);
